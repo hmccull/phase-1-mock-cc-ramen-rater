cc864a9f2ac6437db966c6ae51abf277f055319c
Hannah McCullough <hmac091@gmail.com>
2021-12-02 14:14:18 -0700

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,88 +1,83 @@
-// As a user, I can... 
-// see all ramen images in the div with id of #ramen-menu
-// click on image from the #ramen-menu div and see the info about the ramen inside the #ramen-detail div
-// create a new ramen after submitting the new-ramen form, which should be added to the #ramen-menu div - does not need to be POST'ed 
+// array of objects 
+/*
+DELIVERABLES
+- see all ramen images in div with id of ramen-menu
+- click on an image from the ramen-menu and see it's info in the ramen-detail 
+- create new ramen after submutting new-ramen form  
+*/
+
+// ! GLOBAL VARIABLES 
+// find our menu
+const ramenMenu = document.querySelector('#ramen-menu');
+// find our form 
+const form = document.querySelector('#new-ramen');
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadImages();
+    loadRamenData();
 
-    const form = document.querySelector('#new-ramen');
+    // add event listener to form for submit 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        const newRamenObj = {
-            name: e.target.new_name.value,
-            restaurant: e.target.new_restaurant.value,
-            image: e.target.new_image.value,
-            rating: e.target.new_rating.value,
-            comment: e.target.new_comment.value
+        // set user input equal to key/value pairs in object
+        const ramenObject = {
+            name: `${e.target.new_name.value}`,
+            restaurant: `${e.target.new_restaurant.value}`,
+            image: `${e.target.new_image.value}`,
+            rating: `${e.target.new_rating.value}`,
+            comment: `${e.target.new_comment.value}`,
         }
-        renderRamen(newRamenObj);
+        // pass object to be rendered to DOM
+        renderRamenObject(ramenObject)
+        // reset form after submit event
+        form.reset();
     });
 })
 
-function loadImages() {
+// fetch ramen data 
+function loadRamenData() {
     fetch('http://localhost:3000/ramens')
-    .then(res => res.json())
-    .then(data => data.forEach(ramenData => renderRamen(ramenData)))
+    .then (res => res.json())
+    .then(ramenArray => iterateRamenArray(ramenArray))
 }
 
-// function toPostNewRamen(newRamenObj) {
-//     fetch('http://localhost:3000/ramens', {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify()
-//     })
-//     .then(res => res.json())
-//     .then(newRamen => renderImages(newRamen))
-// }
-
-function renderRamen(ramenData) {
-    // set variables equal to desired elements to manipulate
-    const imageDiv = document.querySelector('#ramen-menu');
-    const imageEl = document.createElement('img');
-
-    // set img src equal to argument
-    imageEl.src = `${ramenData.image}`;
-    imageEl.className = `${ramenData.name}`;
-    imageEl.restaurant = `${ramenData.restaurant}`;
-    imageEl.rating = `${ramenData.rating}`;
-    imageEl.comment = `${ramenData.comment}`
-
-    // append to image div
-    imageDiv.append(imageEl);
-
-    // add event listener to each image 
-    imageEl.addEventListener('click', imageDetailsHandle);
+function iterateRamenArray(ramenArray) {
+    // iterate through ramen array and pass each object within the array to be rendered
+    ramenArray.forEach((ramenObj) => renderRamenObject(ramenObj));
 }
 
-// handle to update featured image when image is clicked 
-function imageDetailsHandle(e) {
-    // set variables equal to desired elements to manipulate
-    const detailImage = document.querySelector('.detail-image');
-    const imageName = document.querySelector('.name');
-    const imageRestaurant = document.querySelector('.restaurant');
-    const imageRating = document.querySelector('#rating-display');
-    const imageComment = document.querySelector('#comment-display');
-
-    // set detail image values equal to selected image values 
-    detailImage.src = `${e.target.src}`;
-    imageName.innerText = `${e.target.className}`;
-    imageRestaurant.innerText = `${e.target.restaurant}`;
-    imageRating.innerText = `${e.target.rating}`;
-    imageComment.innerText = `${e.target.comment}`;
+function renderRamenObject(ramenObj) {
+    // create new img element
+    const ramenEl = document.createElement('img')
+    // set img values equal to argument object values 
+    ramenEl.src = `${ramenObj.image}`;
+    ramenEl.className = `${ramenObj.name}`;
+    ramenEl.restaurant = `${ramenObj.restaurant}`;
+    ramenEl.rating = `${ramenObj.rating}`;
+    ramenEl.comment = `${ramenObj.comment}`;
+    // add event listener to ramen element
+    ramenEl.addEventListener('click', handleDetails)
+    // append ramenEl to ramen menu
+    ramenMenu.append(ramenEl);
 }
 
-
-//------------------------------------------------------------------------
-
-// fetch(`http://localhost:3000/ramens/${newRamenObj}`, {
-//     method: "POST",
-//     headers: {
-//         "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify()
-// })
-// .then(res => res.json())
-// .then(newRamenObj => renderImages(newRamenObj))
+function handleDetails(e){
+    // set var equal to clicked element
+    const ramenClicked = e.target;
+    // find our detail image element 
+    const ramenDetails = document.querySelector('.detail-image');
+    // set element src to ramenClicked src   
+    ramenDetails.src = `${ramenClicked.src}`;
+    ramenDetails.alt = `${ramenClicked.className}`
+    // set element name to ramenClicked name
+    const ramenName = document.querySelector('.name');
+    ramenName.innerText = `${ramenClicked.className}`;
+    // set element restaurant to ramenClicked restaurant 
+    const ramenRestaurant = document.querySelector('.restaurant');
+    ramenRestaurant.innerText = `${ramenClicked.restaurant}`;
+    // set element rating to ramenClicked rating 
+    const ramenRating = document.querySelector('#rating-display');
+    ramenRating.innerText = `${ramenClicked.rating}`;
+    // set element comment to ramenClicked comment
+    const ramenComment = document.querySelector('#comment-display');
+    ramenComment.innerText = `${ramenClicked.comment}`;
+}
